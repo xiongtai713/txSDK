5d60e603d2a8161e674968d1d5ca9453a9cc5b73
liujiao <836921287@qq.com>
2020-10-22 16:02:45 +0800

    增加keystore转公钥工具

--- callsol/keystorech.go
+++ callsol/keystorech.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"log"
 	"pdx-chain/accounts/keystore"
@@ -10,7 +11,16 @@ import (
 )
 
 func main() {
-	key, err := keystore.DecryptKey([]byte(`{"address":"e59dc7aec830883aea7a410f922644b7bfe3722b","crypto":{"cipher":"aes-128-ctr","ciphertext":"32af322c0d4ead7cc2046ab85839d1148a3ba169ca41fd316b6109dd2ce806f7","cipherparams":{"iv":"0bbedb246033270b8bb353f7071abb63"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"7f1bfd8bd117ff6bc3f52cd68b0df88cbde28ee6f4edb6f40308edf694e4e007"},"mac":"3c04dfb6c93265eb26d3eaf3ae36820c7001c440d74ad123bbbd477b5f416243"},"id":"f26b705a-0717-4f06-b5e2-237263ca4778","version":3}`), "466fdd66-e208-4b59-a83e-3f19c03c47f7")
+
+
+	var keyStoreFlag = flag.String("key", "", "keystore")
+	var passwordFlag = flag.String("pw", "", "PassWord")
+	flag.Parse()
+	fmt.Println(*keyStoreFlag)
+	fmt.Println(*passwordFlag)
+	//key, err := keystore.DecryptKey([]byte(`{"address":"e59dc7aec830883aea7a410f922644b7bfe3722b","crypto":{"cipher":"aes-128-ctr","ciphertext":"32af322c0d4ead7cc2046ab85839d1148a3ba169ca41fd316b6109dd2ce806f7","cipherparams":{"iv":"0bbedb246033270b8bb353f7071abb63"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"7f1bfd8bd117ff6bc3f52cd68b0df88cbde28ee6f4edb6f40308edf694e4e007"},"mac":"3c04dfb6c93265eb26d3eaf3ae36820c7001c440d74ad123bbbd477b5f416243"},"id":"f26b705a-0717-4f06-b5e2-237263ca4778","version":3}`), "466fdd66-e208-4b59-a83e-3f19c03c47f7")
+	key, err := keystore.DecryptKey([]byte(*keyStoreFlag), *passwordFlag)
+
 	if err != nil {
 		log.Fatal("错误了", err)
 	}
